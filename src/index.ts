b0313c6f88ecdfb768fc08e08407bb447d27346b
Jaspar Gupta <jaspar.gupta@gmail.com>
2022-06-21 11:41:42 +0200

    feat: export abstract cache driver from index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
+export { default as CacheDriver } from './drivers/driver';
 export { default } from './register';
--- src/register.ts
+++ src/register.ts
@@ -1,12 +1,10 @@
 import type CacheDriver from './drivers/driver';
 
-type Drivers = Record<string, CacheDriver<any>>;
-
-export default function register({ ...drivers }: Drivers, fallback: keyof typeof drivers) {
-  return function cache<Store extends keyof typeof drivers>(store: Store = fallback as Store): typeof drivers[Store] {
+export default function register<Drivers extends Record<string, CacheDriver>>(drivers: Drivers, fallback: keyof Drivers) {
+  return function cache<Store extends keyof Drivers>(store: Store = fallback as Store): Drivers[Store] {
     const driver = drivers[store];
 
-    if (!driver) throw new Error(`Cache driver for [${store}] not found`);
+    if (!driver) throw new Error(`Cache driver for [${store as string}] not found`);
 
     return driver;
   };
