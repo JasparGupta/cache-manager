0b6f152aa4472b890a234cadf86afbeb65b8c14b
William Swannell <will@hirespace.com>
2022-11-16 19:31:23 +0000

    cr

--- src/drivers/types/cached-cookie.ts
+++
@@ -1,6 +0,0 @@
-import Cached from './cached';
-import { CookieValueTypes } from 'cookies-next/lib/types';
-
-export default interface CachedCookie extends Cached {
-  value: CookieValueTypes;
-}
--- src/drivers/types/index.ts
+++ src/drivers/types/index.ts
@@ -1,3 +1,2 @@
 export type { default as Cached } from './cached';
-export type { default as CachedCookie } from './cached-cookie';
 export type { default as Promisable } from './promisable';
